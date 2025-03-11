a118a7c5b34b730f6561a09cc97adacaff8fcbc2
gordon <gordonl@qa.com>
2025-03-11 12:20:30 +0000

    added code to include mollys name in app

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Gordon"));
+        System.out.println(sayHelloToSomeone("Molly"));
         System.out.println(sayGoodbye());
     }
 
